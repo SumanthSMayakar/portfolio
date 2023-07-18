c51f0eec2eedee9e0d0981e7f29748aefeb57789
SumanthSMayakar <sumanthmayakar@gmail.com>
2023-07-18 16:07:47 +0530

    portfolio topbar completed

--- src/App.js
+++ src/App.js
@@ -1,16 +1,17 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Topbar from './components/topbar/Topbar';
 import Intro from './components/intro/Intro';
 import Portfolio from './components/portfolio/Portfolio';
 import Work from './components/works/Work';
-import Contact from './components/contact/Contact'
-import Testimonials from './components/testimonials/Testimonials'
+import Contact from './components/contact/Contact';
+import Testimonials from './components/testimonials/Testimonials';
 import "./app.scss"
 
 function App() {
+  const [menuOpen,setMenuOpen] = useState(false)
   return (
     <div className="app">
-      <Topbar/>
+      <Topbar menuOpen={menuOpen} setMenuOpen={setMenuOpen}/>
       <div className="sections">
         <Intro/>
         <Portfolio/>
