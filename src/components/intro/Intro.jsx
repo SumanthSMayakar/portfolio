266140bdceeb8fe4a9295304defca323ea15aedc
SumanthSMayakar <sumanthmayakar@gmail.com>
2023-07-18 18:28:41 +0530

    intro completed

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -1,9 +1,36 @@
-import React from 'react'
+import React, { useEffect, useRef } from 'react'
 import "./intro.scss"
+import { init } from 'ityped'
+
 
 function Intro() {
+
+  const textRef = useRef()
+  useEffect(() =>{
+    init(textRef.current,{
+       showCursor: false,
+       backDelay: 1500,
+       strings: ['React.js!', 'Yeah!' ] 
+    });
+  },[])
   return (
-    <div className='intro' id='intro'></div>
+    <div className='intro' id='intro'>
+      <div className="left">
+        <div className="imgContainer">
+          <img src="assets/sam2.png" alt="profile image" />
+        </div>
+      </div>
+      <div className="right">
+        <div className="wrapper">
+          <h2>Hi There, I'm</h2>
+          <h1>Sumanth S Mayakar</h1>
+          <h3>Freelance <span ref={textRef}></span></h3>
+        </div>
+        <a href="#portfolio">
+          <img src="assets/down.png" alt="" />
+        </a>
+      </div>
+    </div>
   )
 }
 
